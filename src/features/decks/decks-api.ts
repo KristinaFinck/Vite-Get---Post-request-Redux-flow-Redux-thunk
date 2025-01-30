984daaa36ce22bd65b894fefdc97450767cab733
KristinaFinck <kristina.finck83@gmail.com>
2025-01-30 23:59:22 +0200

    create fetch api with types

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,37 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+export const decksApi = () => {
+   fetchDesks() {
+     return instance.get<any>('v2/desks')
+  }
+}
+export type FetchDecksResponse = {
+  items: Deck[];
+  pagination: Pagination;
+  maxCardsCount: number;
+};
+
+export type Author = {
+  id: string;
+  name: string;
+};
+
+export type Deck = {
+  author: Author;
+  id: string;
+  userId: string;
+  name: string;
+  isPrivate: boolean;
+  cover: string;
+  created: string;
+  updated: string;
+  cardsCount: number;
+};
+
+export type Pagination = {
+  currentPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  totalItems: number;
+};

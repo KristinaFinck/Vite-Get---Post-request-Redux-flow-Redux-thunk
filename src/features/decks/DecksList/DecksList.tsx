89f6b8278842d3448e89aa779cc1552e46ee0752
KristinaFinck <kristina.finck83@gmail.com>
2025-01-31 17:47:40 +0200

    create custom hook useFetchDecks

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,20 +1,10 @@
 import s from './DecksList.module.css'
-import {useEffect} from 'react'
-import { Deck, decksApi } from '../decks-api.ts'
-import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
-import { fetchDecksAC } from '../decks-reducer.ts'
+import { Deck } from '../decks-api.ts'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
-import { selectDecks } from '../decks-selectors.ts'
-import { fetchDecksTC } from '../decks-thunks.ts'
+import { useFetchDecks } from './DeckItem/useFetchDecks.ts'
 
 export const DecksList = () => {
-   const dispatch = useAppDispatch()
-  const decks:Deck[] = useAppSelector(selectDecks)
-
-
-  useEffect(() => {
-      dispatch(fetchDecksTC())
-  }, [])
+const {decks} = useFetchDecks()
   return <ul className={s.list}>
     {decks.map((deck:Deck) => (
         <DeckItem key={deck.id} deck={deck} />
